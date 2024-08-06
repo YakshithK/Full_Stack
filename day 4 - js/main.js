a0864c1936da764d168ed34509476dd94c703223
Yakshith <yakshith.kommineni@gmail.com>
2024-08-06 09:01:15 -0400

    learned js fundamentals

--- day 4 - js/main.js
+++ day 4 - js/main.js
@@ -1,4 +1,4 @@
-// let, const
+/* let, const
 let age = 30;
 age = 31;
 //console.log(age)
@@ -50,10 +50,10 @@ console.log(fruits)
 
 console.log(Array.isArray('hello'))
 console.log(fruits.indexOf('Pears'))
-console.log(fruits)*/
+console.log(fruits)
 
 //object literals
-/*const person = {
+const person = {
     firstName: 'John',
     lastName: 'Doe',
     age: 30,
@@ -100,21 +100,6 @@ console.log(todoJSON)
 
 //Loops
 
-const e = 11
-const b = 100
-
-if(e==10) {
-    console.log('x is 10')
-} else if(e >10){
-    console.log('x is greater than 10')
-} else {
-    console.log('x is NOT 10')
-}
-
-if(e>5&&b > 10){
-    console.log('e is more than 5 and b is more than 10')
-}
-
 //For
 for (let i = 0; i < 10; i++) {
     console.log(`For Loop Number: ${i}`)
@@ -148,4 +133,172 @@ const todoTask = todos.filter(function(todo){
 })
 
 console.log(todoTask)
+
+
+const e = 11
+const b = 100
+
+if(e==10) {
+    console.log('x is 10')
+} else if(e >10){
+    console.log('x is greater than 10')
+} else {
+    console.log('x is NOT 10')
+}
+
+if(e > 5 && b > 10){
+    console.log('e is more than 5 and b is more than 10')
+}
+if(e > 5 || b > 10){
+    console.log('e is more than 5 or b is more than 10')
+}
+
+const x = 11
+const color = x > 10 ? 'red' : 'blue'
+
+switch(color) {
+    case 'red':
+        console.log('color is red')
+        break
+    case 'blue':
+        console.log('color is blue')
+        break
+    default:
+        console.log('color is NOT red or blue')
+        break
+}
+
+
+
+// Functions
+
+function addNums(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addNums(5, 5))
+
+const addNums = (num1=1, num2=1) => num1 + num2
+
+console.log(addNums(1))
+
+
+//Constructor functions
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.dob = new Date(dob)
+}
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear()
+}
+
+Person.prototype.getFirstName = function() {
+    return `${this.firstName} ${this.lastName}`
+}
+
+const person1 = new Person('John', 'Doe', '4-3-1980')
+const person2 = new Person('Mary', 'Smith', '1-8-1880')
+//console.log(person2.dob.getFullYear())
+
+console.log(person1.getBirthYear())
+console.log(person1.getFirstName())
+console.log(person1)
+
+
+//Class
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.dob = new Date(dob)
+    }
+
+    getBirthYear(){
+        return this.dob.getFullYear()
+    }
+
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+const person1 = new Person('John', 'Doe', '4-3-1980')
+const person2 = new Person('Mary', 'Smith', '1-8-1880')
+//console.log(person2.dob.getFullYear())
+
+console.log(person1.getBirthYear())
+console.log(person1.getFullName())
+console.log(person1)
+
+
+//console.log(window)
+
+//Selectors
+
+//Single
+let form = document.getElementById('my-form')
+form = document.querySelector('div')
+console.log(form)
+
+//Multiple
+form = document.querySelectorAll('.item')
+form = document.getElementsByTagName('li')
+console.log(form)
+
+
+const items = document.querySelectorAll('.item')
+items.forEach((item) => console.log(item))
+
+
+const ul = document.querySelector('.items')
+
+//ul.remove()
+
+//ul.firstElementChild.remove()
+//ul.lastElementChild.remove()
+ul.firstElementChild.textContent = 'Hello'
+
+ul.children[1].innerText = 'Brad'
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+const btn = document.querySelector('.btn')
+btn.style.background = 'red'
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    document.querySelector('#my-form').style.background = '#ccc'
+    document.querySelector('body').classList.add('bg-dark')
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+})
 */
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
+
+myForm.addEventListener('submit', onSubmit)
+
+function onSubmit(e){
+    e.preventDefault()
+
+    if (nameInput.value == '' || emailInput.value == '') {
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(() => msg.remove(), 3000)
+    } else {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+
+        userList.appendChild(li)
+
+        // Clear Fields
+        nameInput.value = ''
+        emailInput.value = ''
+    }
+}
