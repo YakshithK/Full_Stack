629406d48ade5db8043409a39861388b90dc185b
Yakshith <yakshith.kommineni@gmail.com>
2024-08-08 19:03:21 -0400

    learned more jsx

--- day 7 - react/react-basics/src/index.js
+++ day 7 - react/react-basics/src/index.js
@@ -47,3 +47,25 @@ import ReactDOM from 'react-dom/client'
 //     <h1 onClick={() => { setText('useState() in action.') }}>{text}</h1>
 //   )
 // }
+
+function App() {
+  const [button, setButton] = useState('Show')
+  const [paragraph, setParagraph] = useState('none')
+
+  function toggleVisibility() {
+    setButton(button === 'Show' ? 'Hide': 'Show');
+    setParagraph(paragraph === 'none' ? 'block': 'none')
+  }
+
+  return (
+    <div>
+    <button onClick={toggleVisibility}>{button}</button>
+    <p style={{ display: paragraph }}>This is some text</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<App/>)
+
+
