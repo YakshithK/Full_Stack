cd81326003f7d928e401176b729958a3c6daf44c
Yakshith <yakshith.kommineni@gmail.com>
2024-08-07 21:57:24 -0400

    learned more npm and added gitignore

--- day 6 - npm/main.js
+++ day 6 - npm/main.js
@@ -1,12 +1,20 @@
-var moment = require('moment')
-var unique = require('uniq')
+// var moment = require('moment')
+// var unique = require('uniq')
 
-const myDate = new Date()
-const myCoolDate = moment(myDate).format('dddd')
+// const myDate = new Date()
+// const myCoolDate = moment(myDate).format('dddd')
 
-const myList = [1, 2, 1, 1, 1, 1, 3, 3, 4, 5, 6, 7, 4]
+// const myList = [1, 2, 1, 1, 1, 1, 3, 3, 4, 5, 6, 7, 4]
 
-const myUniqueList = unique(myList)
+// const myUniqueList = unique(myList)
 
-console.log(myCoolDate)
-console.log(myUniqueList)
+// console.log(myCoolDate)
+// console.log(myUniqueList)
+
+const _ = require('lodash')
+
+const numbers = [33, 46, 75, 44, 32, 3]
+
+_.each(numbers, function(number, i){
+    console.log(number)
+})
